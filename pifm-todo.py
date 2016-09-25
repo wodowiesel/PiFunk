39aad526cb29db1ea38c9f943c5c5ff432103e55
silicator <abx@gmx.de>
2016-09-25 23:10:59 +0200

    add basic files
    to-do list
    pifm basic
    pifunk-main
    sound-test file

---
+++ pifm-todo.py
@@ -0,0 +1,49 @@
+
+
+
+
+
+# TO-DO LIST
+# ----------------------------------------------------------------------
+# 1 different files for chan, imp, funcs (...working on it)
+#
+# 2 Split Frequency Ranges (incl. files) (...working on it)
+#   Input (from 1 MHz to 1000 MHz) 
+#   supported for min. and max. Freq by RPi-1-B+ and other types
+#
+# 3 (see 1) selecting by tree or dropdown /case ?! --> this & Main-Menu 
+# -----------------------------------------------------------------------
+# Functions/ processes: *=working on it
+# supports UKW radio fm/am , ltp , 433, emg, cb*, pmr*, vhf,  etc.
+# 4 AM/FM switch where needed (cb/ham)
+# File player: mp3/wave-Files + playlist 
+# 5 Microphone (usb&jack) / VOX - double mono from devices (jack-cable)
+# 6 Squelch (treshhold) & Volume -> compare system-controls
+# 7 Recording possibilities -> check with codec & implement ffmpg
+# add save-ability (saving last window-radio settings too / memory-mode?)
+# add optional identification-code (nick/user-name)
+#
+# 8 add Internet capabilities like 
+# aprs/ gps(+local if available) etc. (lots of stuff -__-)
+# web-ui support: music playlist, YT, RDS, ts2/3,
+# text2speech: eng&de
+# (small logger maybe for funcs)
+#----------------------------------------------------------------------------------------------
+# 9 GPIO 1,4 (8? special) -> Watch-Monitoring (mVolt & mAmpere & mW) + core-temp 
+# --> log + output 
+#
+# Build: check and combine code (less work :D)!
+#
+# 10 update readme: 
+# add own infos
+# and contributing helpful sources & copyright & licences (...working on it)
+#
+# 11 test & release
+# 12 (+ later GUI GTK+)
+#
+# 13 exception & bug-handling (difficult) and progress-versions
+#
+# make extra branch-fork for testing
+
+
+
---
+++ pifunk-main.py
@@ -0,0 +1,26 @@
+# free Band combo (HAM): listener/transmitter as beacon, gps, internet, relais, aprs by server-client (all-in-one-version)
+# supports UKW radio fm/am , ltp , 433, emg, cb, pmr, vhf, ts2/3, mp3/wave-Files , YT, RDS, microphone (usb&jack) etc.
+
+# Imports
+
+!/usr/bin/python
+
+import time
+
+# Process
+from subprocess import call
+
+
+def play_sound( filename ):
+   call(["./pifm ", filename, freq])
+   return
+   
+# Commands:
+   
+# sudo ./pifm test.wav 103.3 22050 stereo
+
+# play MP3
+# ffmpeg -i input.mp3 -f s16le -ar 22.05k -ac 1 - | sudo ./pifm -(freq and file input)
+
+# Broadcast from a usb microphone
+# arecord -d0 -c2 -f S16_LE -r 22050 -twav -D copy | sudo ./pifm - ?
