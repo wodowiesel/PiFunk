21ce478636c4e6d1dfaf9f5db7f96fb0062e563a
silicator <abx@gmx.de>
2016-10-16 13:26:31 +0200

    travis RPi update 8 +shell
    travis RPi update 8 - copy to shell

--- pifunk-shell.sh
+++ pifunk-shell.sh
@@ -1,9 +1,14 @@
 
 echo
+#- sudo pip install RPi.GPIO
+sudo apt-get install python-rpi.gpio
+#sudo python setup.py install
 # sudo ./pifunk sound.wav 100.0
 # sudo ./pifm sound.wav 100.0
 sudo python pifunk-main.py
 #sudo python pifunk-temp.py
 # sudo python pifunk-pmr.py
 # sudo python pifm.py
+
+
 
