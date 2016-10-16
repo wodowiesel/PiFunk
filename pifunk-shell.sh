2f2d1246f39f4f40fd1bc2a0c1f897420278a9a1
silicator <abx@gmx.de>
2016-10-16 13:45:56 +0200

    travis + shell update 9
    travis + shell update 9

--- pifunk-shell.sh
+++ pifunk-shell.sh
@@ -1,12 +1,14 @@
 
+#pifunk shell-terminal script addinonaly
 echo
-#- sudo pip install RPi.GPIO
+sudo pip install RPi.GPIO
 sudo apt-get install python-rpi.gpio
-#sudo python setup.py install
+sudo python setup.py install
 # sudo ./pifunk sound.wav 100.0
 # sudo ./pifm sound.wav 100.0
 sudo python pifunk-main.py
-#sudo python pifunk-temp.py
+# sudo python pifunk-temp.py
+# sudo python pifunk-cb.py
 # sudo python pifunk-pmr.py
 # sudo python pifm.py
 
