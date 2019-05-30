15ce39e6b821f8955e0bbb5fb9c32efce84a0936
silicator <abx@gmx.de>
2019-05-30 06:00:37 +0200

    Update pifunktest.c

--- pifunktest.c
+++ pifunktest.c
@@ -1688,17 +1688,18 @@ int main (int argc, char **argv) // arguments for global use must! be in main
 					printf ("\nArgument-Error! Use Parameters to run: \n[-n <filename>] [-f <freq>] [-s <samplerate>] [-m <mod (fm/am)>] [-c <callsign (optional)>] [-p <power (0-7>]\n There is also an assistent [-a] or for help [-h]! The *.wav-file must be 16-bit @ 22050 [Hz] Mono \n");
 					return 1;
    		} // end of switch
-			return filename, freq, samplerate, mod, callsign, power;
 
+			printf ("\nChecking File: %s \n", filename);
+			printf ("\nChecking Freq: %f [MHz] \n", freq);
+			printf ("\nChecking Samplerate: %u [Hz] \n", samplerate);
+			printf ("\nChecking Modulation: %s \n", mod);
+			printf ("\nChecking Callsign: %s \n", callsign);
+			printf ("\nChecking Output-Power: %d \n", power);
+			return filename, freq, samplerate, mod, callsign, power;
   	} // end of while
 
    //-- for debugging or information :)
-	 printf ("\nChecking File: %s \n", filename);
-	 printf ("\nChecking Freq: %f [MHz] \n", freq);
-	 printf ("\nChecking Samplerate: %u [Hz] \n", samplerate);
-	 printf ("\nChecking Modulation: %s \n", mod);
-	 printf ("\nChecking Callsign: %s \n", callsign);
-	 printf ("\nChecking Output-Power: %d \n", power);
+
    //printf ("\nArguments(argc): %d / Programm(0): %s / File(1): %s \nFreq(2): %s / Samplerate(3): %s / Modulation(4): %s / Callsign(5): %s / Power(6): %d  \n", argc, argv [0], argv [1], argv [2], argv [3], argv [4], argv [5], argv [6]);
    //printf ("&Adresses-> argc: %p / Name: %p \nFile: %p / Freq: %p \nSamplerate: %p / Modulation: %p / Callsign: %p / Power: %p \n", &argc, &argv [0], &argv [1], &argv [2], &argv [3], &argv [4], &argv [5], &argv [6]);
    //printf ("*Pointers-> argc: %p / Name: %p / File: %p / Freq: %p / Samplerate: %p / Modulation: %p / Callsign: %p / Power: %p  \n", argc, *argv [0], *argv [1], *argv [2], *argv [3], *argv [4], *argv [5], *argv [6]);
