e9970374c542b375832d715b8f5dd7256ec773eb
silicator <abx@gmx.de>
2019-06-08 22:01:56 +0200

    Update pifunk.c

--- pifunk.c
+++ pifunk.c
@@ -530,8 +530,8 @@ volatile unsigned *allof7e;
 #define SUBSIZE                         (1)
 #define DATA_SIZE                       (1000)
 
-#define ACCESS(PERIPH_VIRT_BASE)       (PERIPH_VIRT_BASE + (ALLOF7E - SUB_BASE)) //volatile int* volatile unsigned*
-#define SETBIT(PERIPH_VIRT_BASE, bit)  ACCESS(PERIPH_VIRT_BASE || 1<<bit)// |=
+#define ACCESS(PERIPH_VIRT_BASE)       (PERIPH_VIRT_BASE + ALLOF7E - SUB_BASE)) //volatile int* volatile unsigned*
+#define SETBIT(PERIPH_VIRT_BASE, bit)  ACCESS(PERIPH_VIRT_BASE, 1<<bit)// |=
 #define CLRBIT(PERIPH_VIRT_BASE, bit)  ACCESS(PERIPH_VIRT_BASE) == ~(1<<bit) // &=
 
 //----------------------------------
