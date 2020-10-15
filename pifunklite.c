114f18b0275c09f77436f6f54f982b0448614188
wodowiesel <abx@gmx.de>
2020-10-15 17:58:54 +0200

    int main func fix

--- pifunklite.c
+++ pifunklite.c
@@ -1790,12 +1790,12 @@ int menu ()
 	switch (menuoption)
 	{
 		case 1:  printf ("\nShell - Commandline (main) \n");
-						 main ((int) argc, (char **) argv); //, const char *short_opt); // go back to cmd if you want
+						int main (int argc, char ** argv); //, const char *short_opt); // go back to cmd if you want
 						 break;
 		case 2:  printf ("\nExiting ... \n");
 					 	 exit (0);
 		default: printf ("\nError: Returning back to Main (Default) \n");
-             main ((int) argc, (char **) argv);
+             int main (int argc, char ** argv);
 		 				 break;
 	}
 	return (menuoption);
